8c09b581b2a29ea53190333f847665451455230a
CreeDXD <pclio92@gmail.com>
2024-06-20 17:46:11 +0200

    add complex_card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,11 @@
 import Card from '../components/Card'
+import Complex_Card from '../components/Complex_card'
 export function Main() {
   return (
     <div className="container__main">
       <h1>Main</h1>
-      <Card />
+      {/* <Card /> */}
+      <Complex_Card />
     </div>
   )
 }
