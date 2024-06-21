199682a2e35805b506691791fcfae146ceffd140
CreeDXD <pclio92@gmail.com>
2024-06-21 17:54:31 +0200

    part1 add Navigation

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,13 +2,19 @@ import '../style/css/index.css'
 import { Main } from '../components/Main'
 import { Header } from '../components/Header'
 import { Footer } from '../components/Footer'
+import Navigation from '../components/Navigation_menu'
 
 function App() {
   return (
     <div className="container">
-      <Header />
-      <Main />
-      <Footer />
+      <div className="container--main">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+      <div className="container--navigation">
+        <Navigation />
+      </div>
     </div>
   )
 }
